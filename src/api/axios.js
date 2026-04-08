fb78549cc1f0ec8e14063cc3716c4cbf12eeec63
Dieter <ha89joo@gmail.com>
2026-04-08 16:42:02 +0900

    uri 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 서버의 주소 등의 네트워크를 설정합니다.
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://43.200.160.144:8080/api',
   withCredentials: true,
 });
 
